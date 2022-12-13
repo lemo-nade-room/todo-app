241a026b1bcce5f24092825c3aa3b3897af92460
YoichiroTanaka <lemo.nade.engineer.room@gmail.com>
2022-12-13 17:47:15 +0900

    Awaitからfor文に書き換え

--- app/model/database/repository/DatabaseTodoRepository.scala
+++ app/model/database/repository/DatabaseTodoRepository.scala
@@ -6,28 +6,25 @@ import model.repository.TodoRepository
 import model.database.{Connection, Table}
 import slick.jdbc.MySQLProfile.api._
 import slick.lifted.CanBeQueryCondition.BooleanColumnCanBeQueryCondition
-import java.sql.Timestamp
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class DatabaseTodoRepository extends TodoRepository {
+case class DatabaseTodoRepository() extends TodoRepository {
 
   override def create(categoryId: Long, title: String, body: String): Future[Long] = Connection.db.run {
-    Table.todos += TodoModel(1L, categoryId, title, body, 0, Timestamp, Timestamp)
-  }.map(_.toLong)
+    Table.todos returning Table.todos.map(_.id) += TodoModel(0, categoryId, title, body, 0)
+  }
 
   override def update(todoId: Long, title: Option[String], body: Option[String], state: Option[TodoStateContent], categoryId: Option[Long]): Future[Unit] = {
-    for (old <- Connection.db.run(Table.todos.filter(_.id == todoId).result.head)) yield {
-      val deleteAction = Table.todos.filter(_.id == todoId).delete
-      val createAction = Table.todos += TodoModel(
+    for (old <- Connection.db.run(Table.todos.filter(_.id === todoId).result.head)) yield {
+      val deleteAction = Table.todos.filter(_.id === todoId).delete
+      val createAction = Table.todos.forceInsert(TodoModel(
         todoId,
         categoryId.getOrElse(old.categoryId),
         title.getOrElse(old.title),
         body.getOrElse(old.body),
         state.getOrElse(TodoStateContent(old.state)).state,
-        Timestamp,
-        Timestamp
-      )
+      ))
       for (_ <- Connection.db.run((deleteAction andThen createAction).transactionally)) yield Unit
     }
   }
