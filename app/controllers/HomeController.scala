0fe7b735d4d25cdbf5fbbdb4f82746bceaa7f3a4
YoichiroTanaka <lemo.nade.engineer.room@gmail.com>
2022-12-13 18:45:06 +0900

    新規作成を作りかけ

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,7 +3,10 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import model.ViewValueHome
-import model.database.repository.DatabaseCategoryRepository
+import model.database.repository.{DatabaseCategoryRepository, DatabaseTodoRepository}
+import play.api.data.Form
+import play.api.data.Forms.{longNumber, mapping, number, text}
+
 import java.util.concurrent.TimeUnit
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -19,5 +22,20 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     )
     val categories = Await.result(DatabaseCategoryRepository().all(), Duration(10, TimeUnit.SECONDS))
     Ok(views.html.Home(vv, categories))
+  }
+
+  // TODO: 新規作成はできるがindexのViewが返せない
+  def create(): Action[AnyContent] = Action { implicit req =>
+    case class CreateTodo(title: String, body: String, categoryId: Long)
+    val form = Form(
+      mapping(
+        "title" -> text,
+        "body" -> text,
+        "categoryId" -> longNumber
+      )(CreateTodo.apply)(CreateTodo.unapply)
+    )
+    val create = form.bindFromRequest().get
+    Await.ready(DatabaseTodoRepository().create(create.categoryId, create.title, create.body), Duration(10, TimeUnit.SECONDS))
+    return index()
   }
 }
