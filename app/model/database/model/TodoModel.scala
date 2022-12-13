5e4a8ae04caeb27fed5e5de7c66a87b49f901336
YoichiroTanaka <lemo.nade.engineer.room@gmail.com>
2022-12-13 17:45:41 +0900

    Timestampのデフォルト値に現在時刻を設定

--- app/model/database/model/TodoModel.scala
+++ app/model/database/model/TodoModel.scala
@@ -3,16 +3,17 @@ package model.database.model
 import model.content.{TodoContent, TodoStateContent}
 
 import java.sql.Timestamp
+import java.time.LocalDateTime
 
 case class TodoModel
 (
-  id: Long,
+  id: Long = 0,
   categoryId: Long,
   title: String,
   body: String,
   state: Int,
-  updatedAt: Timestamp,
-  createdAt: Timestamp,
+  updatedAt: Timestamp = Timestamp.valueOf(LocalDateTime.now()),
+  createdAt: Timestamp = Timestamp.valueOf(LocalDateTime.now()),
 ) {
   def convert: TodoContent = TodoContent(id, title, body, TodoStateContent(state))
 }
